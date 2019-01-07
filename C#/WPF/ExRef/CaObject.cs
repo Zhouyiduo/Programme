f91579acd9454234e1cb89c4c3f21d08d87419ab
Mcc <896894913@qq.com>
2019-01-07 14:47:15 +0800

    修改
    修改

---
+++ C#/WPF/ExRef/CaObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExRef
+{
+
+    public class CaSubObject
+    {
+        public int m1;
+        public int m2;
+        public string m3;
+    }
+
+    public class CaObject
+    {
+        public CaObject(int m1,int m2,string m3)
+        {
+            M1 = m1;
+            M2 = m2;
+            M3 = m3;
+        }
+        public CaObject(){}
+
+        public int M1;
+        public int M2;
+        public string M3;
+    }
+}
---
+++ C#/WPF/ExRef/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CaObject obj= new CaObject(1,1,"S1");
+            test1(obj);
+            return;
+        }
+
+
+        static void test1(CaObject obj1)
+        {
+            List<CaObject> obj1New = new List<CaObject>();
+            obj1New.Add(obj1);
+            obj1New[0].M1 = 1000;
+        }
+
+
+        static void test2(CaObject obj2)
+        {
+
+        }
+
+
+    }
+}
